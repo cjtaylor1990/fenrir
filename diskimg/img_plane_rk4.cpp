5654916d8313e0cd4a234bf40ce9d911d88d5b3f
Corbin Taylor <cjtaylor@astro.umd.edu>
2018-08-01 12:44:39 -0400

    This is my initial upload of the Fenrir disk image integrator, as it is of today (8-1-2018). This uses a Cartesian grid for the image plane and an RK4 integrator. This is what was used for the first Fenrir paper.
    -Corbin

---
+++ diskimg/img_plane_rk4.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include "ic1_rk4_thindisk.h"
+#include "photon_geos_thindisk.h"
+#include "metric_kerr.h"
+#include "matter_geos_thindisk.h"
+#include "propagate_rk4_thindisk.h"
+
+
+int main(int argc, char* argv[]){
+    //Opening file that will be the output
+    std::ofstream myfile; //opening i/o stream to output to 'myfile' named in ic file
+    myfile.open (outFileName); //opening a new txt file 'myfile' named in ic file
+    //Calculating radius of event horizon
+    double rEvent = 1. - pow(1.-(a*a),0.5);
+    //Looping over NxN image plane
+    int j,k; //defining looping dummy variables. j is x variable. k is y variable
+    double imgX,imgY,imgB; //defining x,y cartesian grid for image plane. imgX = x, imgY = y, and imgB = 2-D distance (sqrt(x^2 + y^2))
+    double scaleHeightValue; //vertical height of disk above the midplane
+    double rProjected; //pseudo-cylindrical radius
+    j = 0; //setting dummy j to 0
+    while (j < n){
+        k = 0; //setting dummy k to 0 (gets set back to 0 for each j loop)
+        while (k < n){
+            imgX = ((j - (n/2.))+0.5)*(imgSize/n); //calculating x value for j value, could always do it outside of loop to not have to constantly do calculation
+            imgY = ((k - (n/2))+0.5)*(imgSize/n); //calculating y value for k value
+            imgB = pow(pow(imgX,2.)+pow(imgY,2.),0.5); //calculating Euclidean 2-D distance on image plane
+            
+            //calculating the phi angular momentum and carter constant of photon from image plane coordinates
+            angmom = -1.*imgB*std::sin(alpha)*(imgX/imgB); //phi angular momentum
+            carter = pow(imgB*(imgY/imgB),2.) - energy*pow(a*std::cos(initTheta),2.) + pow(angmom*std::cos(initTheta)/std::sin(initTheta), 2.); //carter constant
+
+			//setting the switches for the sign of rdot and thetadot
+            rSqrtSwitch = 1.;
+            if (imgY < 0.){
+                thSqrtSwitch = -1.;
+            }else{
+                thSqrtSwitch = 1.;
+            }
+			
+			//setting the initial values of the photon's position 4-vector to be found in the ic file
+            posVec[0] = initTime; //time
+            posVec[1] = initRadius; //spherical radius
+            posVec[2] = initTheta; //vertical angle theta
+            posVec[3] = initPhi; //horizontal angle phi
+
+            //Propagate the photon from the observer to the disk
+            propagate(posVec,momVec,dStep,tolerance,maxStep,rLimit,rEvent,scaleHeightValue,rProjected);
+
+            //Calculating the pseudo-cylindrical radius and the vertical height of the disk above the mid-plane
+            rProjected = posVec[1]*std::sin(posVec[2]); //pseudo-cylindrical radius
+            scaleHeightValue = heightFrontTerm*(1 - sqrt(rIsco/rProjected)); //vertical height of the disk above mid-plane
+
+            //Calculating the one-form of the photon's momentum 4-vector
+            double momOneForm[4]; //defining momentum 4-vector
+            momOneForm[0] = (gTT(posVec[1],posVec[3],posVec[2],posVec[0])*momVec[0])+(gTPh(posVec[1],posVec[3],posVec[2],posVec[0])*momVec[3]); //time
+            momOneForm[1] = (gRR(posVec[1],posVec[3],posVec[2],posVec[0])*momVec[1]); //radius
+            momOneForm[2] = (gThTh(posVec[1],posVec[3],posVec[2],posVec[0])*momVec[2]); //theta
+            momOneForm[3] = (gPhT(posVec[1],posVec[3],posVec[2],posVec[0])*momVec[0])+(gPhPh(posVec[1],posVec[3],posVec[2],posVec[0])*momVec[3]); //phi
+
+            //Calculating the disk's velocity 4-vector
+            double diskVelVec[4]; //defining disk's 4-velocity
+            diskVelVec[0] = diskTdot(posVec[1],posVec[3],posVec[2],posVec[0],scaleHeightValue,rProjected); //time
+            diskVelVec[1] = diskRdot(posVec[1],posVec[3],posVec[2],posVec[0],scaleHeightValue,rProjected); //radius
+            diskVelVec[2] = diskThDot(posVec[1],posVec[3],posVec[2],posVec[0],scaleHeightValue,rProjected); //theta
+            diskVelVec[3] = diskPhiDot(posVec[1],posVec[3],posVec[2],posVec[0],scaleHeightValue,rProjected); //phi
+
+            //Calculating the final energy of the photon (E = -p*U, dotting photon 4-momentum with disk 4-velocity)
+            double finalEnergy = (momOneForm[0]*diskVelVec[0]) + (momOneForm[1]*diskVelVec[1]) + (momOneForm[2]*diskVelVec[2]) + (momOneForm[3]*diskVelVec[3]);
+            finalEnergy = -1.*finalEnergy;
+			
+			//Outputting data to 'myfile': x, y, g, final_t, final_r, final_theta, final_phi, disk_H, pseudo-cylindrical_r
+            myfile << imgX << " " << imgY << " " << (energy/finalEnergy) << " " << posVec[0] << " " << posVec[1] << " " << posVec[2] << " " << posVec[3] << " " << scaleHeightValue << " " << rProjected << "\n";
+            
+            //advancing k by 1
+            k++;
+        }
+        //advancing j by 1
+        j++;
+    }
+    //closing 'myfile' to be saved in path defined in ic file
+    myfile.close();
+    return 0;  //returning integer value of 0 if executed to completion
+
+}  //end of main function
