772d95c43a4763243f7413b985bde1fff13c6943
Corbin Taylor <cjtaylor@astro.umd.edu>
2019-02-27 20:12:35 -0500

    Update 2/27/19 #1
    I'm adding the automated scripts that automatically create lineprofile model data from Fenrir and RELXILL. THe .py script compares them.

---
+++ transfer/grid_compare_datacreate_line_fenrir.sh
@@ -0,0 +1,39 @@
+param_file="/Users/cjtaylor/sparse_grid/param_file_converted.txt"
+output_path="/Users/cjtaylor/sparse_grid/fenrir_lines"
+
+heainit
+
+comb_index=1
+while INF=' ' read -ra line; do
+	spin_val=${line[0]}
+	inc_val=${line[1]}
+
+	xspec <<EOF
+	lmod relxill ./
+	dummy 0.002 2.002 1000 linear
+	model relline
+	1
+	3
+	3
+	30
+	${spin_val}
+	${inc_val}
+	-1
+	80
+	0
+	0
+	1
+	cpd /xs
+	pl eemod
+	ipl
+	wdata ${output_path}/fenrir_model_${comb_index}
+	exit
+	exit
+	EOF
+	
+	tail -n +4 ${output_path}/fenrir_model_${comb_index}.qdp > ${output_path}/fenrir_model_${comb_index}.txt
+	rm -f ${output_path}/fenrir_model_${comb_index}.qdp
+	python /Users/cjtaylor/sparse_grid/txt_to_npy_models.py ${output_path}/fenrir_model_${comb_index}.txt ${output_path}/fenrir_model_${comb_index}.npy
+	rm -f ${output_path}/fenrir_model_${comb_index}.txt
+	comb_index=$((comb_index+1))
+done < $param_file
