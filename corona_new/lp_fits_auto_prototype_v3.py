74d8b2b2aa2451da05140a8f47a9a6211026dec2
Corbin Taylor <cjtaylor@astro.umd.edu>
2019-06-27 13:36:07 -0400

    Created a new version of lp_fits_auto_prototype_v3.py. I also have a working version of lp_fits_combine_prototype_v1.py. I could possibly add in an additional bit to a lp_fits_combine_prototype to include adding on radial, height, and spin values as a function of row for each HDU.

---
+++ corona_new/lp_fits_auto_prototype_v3.py
@@ -0,0 +1,66 @@
+import numpy as np
+from astropy.io import fits
+import sys
+import os
+
+#Input file information
+inputPrefix = str(sys.argv[1])
+numberInputFiles = int(sys.argv[2])
+inputFiles = [inputPrefix + '_' + str(i) + '.npy' for i in range(numberInputFiles)]
+
+#Reading input parameters from command line
+outputPrefix = str(sys.argv[3]) #Output FITS file prefix
+heightIndex = int(sys.argv[4])
+outFile = outputPrefix + '_' + str(heightIndex) + '.fits'
+
+#Checking to see if file already exists
+existBool = os.path.isfile(outFile)
+
+a = [1 for i in range(100)]
+b = [2 for i in range(100)]
+c = [3 for i in range(100)]
+
+#If the file doesn't exist
+if not existBool:
+	aArray = [a]
+	bArray = [b]
+	cArray = [c]
+	col1 = fits.Column(name = 'a', format = '100E', unit = 'n/a', array = aArray)
+	col2 = fits.Column(name = 'b', format = '100E', unit = 'n/a', array = bArray)
+	col3 = fits.Column(name = 'c', format = '100E', unit = 'n/a', array = cArray)
+	cols = fits.ColDefs([col1,col2,col3])
+	hdu = fits.BinTableHDU.from_columns(cols)
+	hdu.writeto(outFile)
+	del hdu
+
+	fitsData = fits.open(outFile)
+
+	for file in inputFiles[1:]:
+		aArray = [a]
+		bArray = [b]
+		cArray = [c]
+		col1 = fits.Column(name = 'a', format = '100E', unit = 'n/a', array = aArray)
+		col2 = fits.Column(name = 'b', format = '100E', unit = 'n/a', array = bArray)
+		col3 = fits.Column(name = 'c', format = '100E', unit = 'n/a', array = cArray)
+		cols = fits.ColDefs([col1,col2,col3])
+		fitsData.append(fits.BinTableHDU.from_columns(cols))
+
+	fitsData.writeto(outFile,overwrite=True)
+
+else:
+	fitsData = fits.open(outFile)
+	for i in range(1,len(inputFiles)+1):
+		currentHDU = fitsData[i].data
+		aArray = [currentHDU[j][0] for j in range(len(currentHDU))]#.append(np.array(a,dtype='float32'))
+		bArray = [currentHDU[j][1] for j in range(len(currentHDU))]#.append(np.array(b,dtype='float32'))
+		cArray = [currentHDU[j][2] for j in range(len(currentHDU))]#.append(np.array(c,dtype='float32'))
+		aArray.append(np.array(b,dtype='float32'))
+		bArray.append(np.array(c,dtype='float32'))
+		cArray.append(np.array(a,dtype='float32'))
+		col1 = fits.Column(name = 'a', format = '100E', unit = 'n/a', array = aArray)
+		col2 = fits.Column(name = 'b', format = '100E', unit = 'n/a', array = bArray)
+		col3 = fits.Column(name = 'c', format = '100E', unit = 'n/a', array = cArray)
+		cols = fits.ColDefs([col1,col2,col3])
+		currentHDU = fits.BinTableHDU.from_columns(cols)
+		fitsData[i] = currentHDU
+	fitsData.writeto(outFile,overwrite=True)
--- corona_new/lp_fits_combine_prototype_v1.py
+++ corona_new/lp_fits_combine_prototype_v1.py
@@ -5,60 +5,42 @@ import os
 
 #Input file information
 inputPrefix = str(sys.argv[1])
-numberInputFiles = int(sys.argv[2])
-inputFiles = [inputPrefix + str(i) + '.npy' for i in range(numberInputFiles)]
+numberHeights = int(sys.argv[2])
+numberThickness = int(sys.argv[3])
+inputFiles = [inputPrefix + '_' + str(i) + '.fits' for i in range(numberHeights)]
 
 #Reading input parameters from command line
-outFile = str(sys.argv[3]) #Output FITS files
+outFile = str(sys.argv[4]) #Output FITS files
 
-#Checking to see if file already exists
-existBool = os.path.isfile(outFile)
-
-a = [1 for i in range(100)]
-b = [2 for i in range(100)]
-c = [3 for i in range(100)]
-
-#If the file doesn't exist
-if not existBool:
-	aArray = [a]
-	bArray = [b]
-	cArray = [c]
-	col1 = fits.Column(name = 'a', format = '100E', unit = 'n/a', array = aArray)
-	col2 = fits.Column(name = 'b', format = '100E', unit = 'n/a', array = bArray)
-	col3 = fits.Column(name = 'c', format = '100E', unit = 'n/a', array = cArray)
-	cols = fits.ColDefs([col1,col2,col3])
+for i in range(1,numberThickness+1):
+	aList = []
+	bList = []
+	cList = []
+	for file in inputFiles:
+		#print(file)
+		currentHDU = fits.open(file)
+		#print(currentHDU)
+		currentHDU = currentHDU[i].data
+		aArray = [[row[0] for row in currentHDU]]
+		bArray = [[row[1] for row in currentHDU]]
+		cArray = [[row[2] for row in currentHDU]]
+		aList += aArray
+		bList += bArray
+		cList += cArray
+	print(aList[0])
+	aColumnList = [fits.Column(name='a{}'.format(k+1),format='100E', unit = 'n/a', array = aList[k]) for k in range(numberHeights)]
+	bColumnList = [fits.Column(name='b{}'.format(k+1),format='100E', unit = 'n/a', array = bList[k]) for k in range(numberHeights)]
+	cColumnList = [fits.Column(name='c{}'.format(k+1),format='100E', unit = 'n/a', array = cList[k]) for k in range(numberHeights)]
+	cols = fits.ColDefs(aColumnList + bColumnList + cColumnList) #appending the column lists together
 	hdu = fits.BinTableHDU.from_columns(cols)
-	hdu.writeto(outFile)
-	del hdu
 
-	fitsData = fits.open(outFile)
-
-	for file in inputFiles[1:]:
-		aArray = [a]
-		bArray = [b]
-		cArray = [c]
-		col1 = fits.Column(name = 'a', format = '100E', unit = 'n/a', array = aArray)
-		col2 = fits.Column(name = 'b', format = '100E', unit = 'n/a', array = bArray)
-		col3 = fits.Column(name = 'c', format = '100E', unit = 'n/a', array = cArray)
-		cols = fits.ColDefs([col1,col2,col3])
-		fitsData.append(fits.BinTableHDU.from_columns(cols))
-
-	fitsData.writeto(outFile,overwrite=True)
-
-else:
-	fitsData = fits.open(outFile)
-	for i in range(1,len(inputFiles)+1):
-		currentHDU = fitsData[i].data
-		aArray = [currentHDU[j][0] for j in range(len(currentHDU))]#.append(np.array(a,dtype='float32'))
-		bArray = [currentHDU[j][1] for j in range(len(currentHDU))]#.append(np.array(b,dtype='float32'))
-		cArray = [currentHDU[j][2] for j in range(len(currentHDU))]#.append(np.array(c,dtype='float32'))
-		aArray.append(np.array(b,dtype='float32'))
-		bArray.append(np.array(c,dtype='float32'))
-		cArray.append(np.array(a,dtype='float32'))
-		col1 = fits.Column(name = 'a', format = '100E', unit = 'n/a', array = aArray)
-		col2 = fits.Column(name = 'b', format = '100E', unit = 'n/a', array = bArray)
-		col3 = fits.Column(name = 'c', format = '100E', unit = 'n/a', array = cArray)
-		cols = fits.ColDefs([col1,col2,col3])
-		currentHDU = fits.BinTableHDU.from_columns(cols)
-		fitsData[i] = currentHDU
-	fitsData.writeto(outFile,overwrite=True)
+	if i == 1:
+		if os.path.isfile(outFile): #Checking to see if file already exists
+			print("ERROR! File already exits!")
+			sys.exit()
+		else:
+			hdu.writeto(outFile)
+	else:
+		fitsData = fits.open(outFile)
+		fitsData.append(hdu)
+		fitsData.writeto(outFile,overwrite=True)
