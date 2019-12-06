3eab43616642cb5d7815c061f33ef247206ed7a6
Corbin Taylor <cjtaylor@astro.umd.edu>
2019-12-06 17:03:05 -0500

    Started structure of script that will rename disk image files for LP line profile creation.

--- lineProfLpTests/renameDiskFiles.sh
+++ lineProfLpTests/renameDiskFiles.sh
@@ -1,2 +1,26 @@
 machine="Loki"
 diskDirPath="/Volumes/${machine}/multi_grid_test_data/ongrid_test/"
+
+numSpins=3
+numInclinations=3
+numDiskThicknesses=11
+
+spinIndex=0
+while [ $spinIndex -lt $numSpins ]; do
+    
+    inclinationIndex=0
+	while [ $inclinationIndex -lt $numInclinations ]; do
+
+        diskThicknessIndex=0
+		while [ $diskThicknessIndex -lt $numDiskThicknesses ]; do
+
+            diskThicknessIndex=$((diskThicknessIndex+1))
+        done
+
+        inclinationIndex=$((inclinationIndex+1))
+
+    done
+
+    spinIndex=$((spinIndex+1))
+
+done
