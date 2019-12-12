87bb60b9e8f1602c5d2ca5380a291f0b9d22cbfa
Corbin Taylor <cjtaylor@astro.umd.edu>
2019-12-12 00:31:38 -0500

    Created a first draft at the script that will be used to create the LP+thickDisk grid test data (lineProfLpTests/create_grid_full_lp_test_data.sh). I discovered that XSPEC uses a tcl interpreter, and thus I can write loops but haven't done so yet. This would be ideal, as I would have to load the Gb-sized Fenrir disk grid only once in such a scenario. This will have to be done before doing a full run.

---
+++ lineProfLpTests/create_grid_full_lp_test_data.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+inPath="/Users/cjtaylor/fenrir_full_tests/tests"
+outPath="/Users/cjtaylor/data"
+
+spinParamFile="${inPath}/full_lp_thickdisk_spin_params.txt"
+heightParamFile="${inPath}/full_lp_thickdisk_height_params.txt"
+inclinationParamFile="${inPath}/full_lp_thickdisk_inc_converted_params.txt"
+diskThicknessParamFile="${inPath}/full_lp_thickdisk_thickness_params.txt"
+
+numSpins=3
+numInclinations=3
+numHeights=11
+numDiskThicknesses=11
+
+HEADAS="/Users/cjtaylor/heasoft-6.21/x86_64-apple-darwin16.6.0"
+. $HEADAS/headas-init.sh
+
+spinIndex=0
+while INF=' ' read -ra spinLine; do
+	spin=${spinLine[0]}
+
+	inclinationIndex=0
+	while INF=' ' read -ra inclinationLine; do
+		inclination=${inclinationLine[0]}
+
+		heightIndex=0
+		while INF=' ' read -ra heightLine; do
+			height=${heightLine[0]}
+
+			diskCombinationIndex=$((inclinationIndex + spinIndex*numInclinations))
+			lpCombinationIndex=$((heightIndex + spinIndex*numHeights))
+
+			diskThicknessIndex=0
+			while INF=' ' read -ra thicknessLine; do
+                diskThickness=${thicknessLine[0]}
+
+				qdpFile="${outPath}/lp_grid_disk${diskCombinationIndex}_lp${lpCombinationIndex}_thickness${diskThicknessIndex}.qdp"
+				txtFile="${outPath}/lp_grid_disk${diskCombinationIndex}_lp${lpCombinationIndex}_thickness${diskThicknessIndex}.txt"
+				npyFile="${outPath}/lp_grid_disk${diskCombinationIndex}_lp${lpCombinationIndex}_thickness${diskThicknessIndex}.npy"
+		
+				xspec <<-EOF
+				lmod relxill ./
+				dummy 0.1 100.0 10000
+				model fenconv_lp(xillver)
+				${height}
+				${spin}
+				${inclination}
+                ${diskThickness}
+				-1
+				100
+				0
+				2
+				2
+				3.7
+				300
+				1.0
+				0
+				${inclination}
+				-1
+				1
+				cpd /xs
+				pl eem
+				ipl
+				wdata ${outPath}/lp_grid_disk${diskCombinationIndex}_lp${lpCombinationIndex}_thickness${diskThicknessIndex}
+				exit
+				exit
+				EOF
+
+				tail -n +4 ${qdpFile} > ${txtFile}
+				rm -f ${qdpFile}
+				python2 /Users/cjtaylor/fenrir_sparse_tests/txt_to_npy_models.py ${txtFile} ${npyFile}
+				rm -f ${txtFile}
+				
+				diskThicknessIndex=$((diskThicknessIndex+1))
+			
+			done < $diskThicknessParamFile
+
+			heightIndex=$((heightIndex+1))
+
+		done < $heightParamFile
+
+		inclinationIndex=$((inclinationIndex+1))
+		
+	done < $inclinationParamFile
+
+	spinIndex=$((spinIndex+1))
+
+done < $spinParamFile
