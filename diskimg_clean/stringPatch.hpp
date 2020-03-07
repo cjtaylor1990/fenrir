9a6d3a5dae1e5378ae65db78808e02fa154c0295
Corbin Taylor <cjtaylor@astro.umd.edu>
2020-03-06 22:49:52 -0500

    Cleaned up the stringPatch.hpp file to be consistent with the others, putting the implementation in stringPatch.cpp.

--- diskimg_clean/stringPatch.hpp
+++ diskimg_clean/stringPatch.hpp
@@ -1,12 +1,9 @@
 //This allows me to convert an integer into a string, overcoming the version problems on galaxy (outdated icc compiler)
 #include <string>
 #include <sstream>
-namespace string_patch
-{
-    template < typename T > std::string to_string( const T& nInput )
-    {
-        std::ostringstream stm ;
-        stm << nInput ;
-        return stm.str() ;
-    }
+
+namespace stringPatch {
+    
+    template <typename T> std::string toString(const T& input);
+
 }
