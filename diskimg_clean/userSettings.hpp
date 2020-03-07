3e4ce0bdf0683e0276b23125cb3f1085b1280ff4
Corbin Taylor <cjtaylor@astro.umd.edu>
2020-03-07 00:02:19 -0500

    Finishing userSettings files. This includes a namespace by the same name that includes things that the user might want to change, such as the tolerance, radius of observer, etc.

---
+++ diskimg_clean/userSettings.hpp
@@ -0,0 +1,16 @@
+#ifndef USERSETTINGS_H // include guard
+#define USERSETTINGS_H
+
+namespace userSettings {
+    
+    double initTime;
+    double initRadius;
+    double initPhi;
+
+    double maxStepNumber;
+    double horizonStop;
+    double tolerance;
+
+}
+
+#endif //userSettings
