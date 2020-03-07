1331c74827f1d6170405040cb3c61cf93920771b
Corbin Taylor <cjtaylor@astro.umd.edu>
2020-03-07 00:00:35 -0500

    Cleaning up stringPatch.hpp a bit.

--- diskimg_clean/stringPatch.hpp
+++ diskimg_clean/stringPatch.hpp
@@ -1,6 +1,5 @@
 //This allows me to convert an integer into a string, overcoming the version problems on galaxy (outdated icc compiler)
 #include <string>
-#include <sstream>
 
 namespace stringPatch {
     
